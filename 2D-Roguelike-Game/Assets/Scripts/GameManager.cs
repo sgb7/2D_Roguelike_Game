11e31297f5e608c85937c1af5b51cbbf4d6753a2
Summer <sgbanister@gmail.com>
2020-02-05 10:14:34 -0800

    Edited 2D_Roguelike_Game

--- 2D-Roguelike-Game/Assets/Scripts/GameManager.cs
+++ 2D-Roguelike-Game/Assets/Scripts/GameManager.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance = null;
+    private BoardManager boardScript; // Unity doesn't like this line for some reason?
+    private int level = 3;
+    void Awake()
     {
-        
+        if (instance == null)
+            instance = this;
+        else if (intstance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
     }
 
-    // Update is called once per frame
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
     void Update()
     {
-        
+
     }
 }
