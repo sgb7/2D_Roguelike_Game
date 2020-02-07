1a61ac253e9a4da84791134420f518463854c901
Summer <sgbanister@gmail.com>
2020-02-07 08:16:24 -0800

    Edited 2D_Roguelike_Game

--- 2D-Roguelike-Game/Assets/Scripts/BoardManager.cs
+++ 2D-Roguelike-Game/Assets/Scripts/BoardManager.cs
@@ -1,6 +1,7 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 using System;
-using System.Collections.Generic;         //Allows us to use Lists.
 using Random = UnityEngine.Random;         //Tells Random to use the Unity Engine random number generator.
 
 namespace Completed
