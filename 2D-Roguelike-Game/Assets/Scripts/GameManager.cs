4f97beff5ead10ea980fd9815aba0376ff8cc529
Summer <sgbanister@gmail.com>
2020-02-07 09:33:52 -0800

    Edited 2D_Roguelike_Game

--- 2D-Roguelike-Game/Assets/Scripts/GameManager.cs
+++ 2D-Roguelike-Game/Assets/Scripts/GameManager.cs
@@ -5,10 +5,14 @@ using Completed;
 
 public class GameManager : MonoBehaviour
 {
-
+    public float turnDelay = .1f;
     public static GameManager instance = null;                //Static instance of GameManager which allows it to be accessed by any other script.
-    private BoardManager boardScript;                        //Store a reference to our BoardManager which will set up the level.
-    private int level = 3;                                    //Current level number, expressed in game as "Day 1".
+    private BoardManager boardScript;
+    public int playerFoodPoints = 100;
+    [HideInInspector] public bool playersTurn = true;
+    private int level = 3;
+    private List<Enemy> enemies;
+    private bool enemiesMoving;
 
     //Awake is always called before any Start functions
     void Awake()
@@ -28,6 +32,8 @@ public class GameManager : MonoBehaviour
         //Sets this to not be destroyed when reloading scene
         DontDestroyOnLoad(gameObject);
 
+        enemies = new List<Enemy>();
+
         //Get a component reference to the attached BoardManager script
         boardScript = GetComponent<BoardManager>();
 
@@ -39,15 +45,45 @@ public class GameManager : MonoBehaviour
     void InitGame()
     {
         //Call the SetupScene function of the BoardManager script, pass it current level number.
+        enemies.Clear();
         boardScript.SetupScene(level);
 
     }
 
-
+    public void GameOver()
+    {
+        enabled = false;
+    }
 
     //Update is called every frame.
     void Update()
     {
+        if (playersTurn || enemiesMoving)
+            return;
+        StartCoroutine(MoveEnemies());
+    }
 
+    public void AddEnemyToList(Enemy script)
+    {
+        enemies.Add(script);
+    }
+
+    IEnumerator MoveEnemies()
+    {
+        enemiesMoving = true;
+        yield return new WaitForSeconds(turnDelay);
+        if(enemies.Count == 0)
+        {
+            yield return new WaitForSeconds(turnDelay);
+        }
+
+        for(int i = 0; i < enemies.Count; i++)
+        {
+            enemies[i].MoveEnemy();
+            yield return new WaitForSeconds(enemies[i].moveTime);
+        }
+
+        playersTurn = true;
+        enemiesMoving = false;
     }
 }
